157d2304efcc4ecde6a37befd69215c0604a4cf6
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-07-01 00:28:02 +0200

    add background, foreground, cursor color

--- itermcolors2Xdefaults.rb
+++ itermcolors2Xdefaults.rb
@@ -17,11 +17,10 @@ hash = Plist::parse_xml(ARGV[0])
 
 key = ARGV[1] || "Rxvt"
 
+puts "#{key}*background:   #{as_default(hash["Background Color"])}"
+puts "#{key}*foreground:   #{as_default(hash["Foreground Color"])}"
+puts "#{key}*cursorColor:   #{as_default(hash["Cursor Color"])}"
+
 (0..15).each do |n|
   puts "#{key}*color#{n}:   #{as_default(hash["Ansi #{n} Color"])}"
 end
-
-
-
-
-
